99f6384ebecf98cbf4ce2a6904ad029978fb96b8
alzu2 <alzu2500@gmail.com>
2023-12-11 18:27:07 +0100

    Fxml Starting Page Test 2: Now working

---
+++ src/main/java/com/application/gameshelf/StartingPage.java
@@ -0,0 +1,35 @@
+package com.application.gameshelf;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class StartingPage extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+    // Load the FXML file
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Starting-Page.fxml"));
+        Parent root = fxmlLoader.load();
+
+    // Set the FXML content as the root of the scene
+        Scene scene = new Scene(root, 1440, 1024);
+
+    // Set the scene to the stage
+        stage.setScene(scene);
+
+    // Set other stage properties if needed
+        stage.setTitle("Your Title");
+
+    // Show the stage
+        stage.show();
+}
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
