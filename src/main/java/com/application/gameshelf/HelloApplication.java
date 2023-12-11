257ed1e6a742b98f4157526f04bd2c64d655e1ce
alzu2 <alzu2500@gmail.com>
2023-12-11 17:45:24 +0100

    Fxml Starting Page Test - Deleted

--- src/main/java/com/application/gameshelf/HelloApplication.java
+++ src/main/java/com/application/gameshelf/HelloApplication.java
@@ -2,22 +2,34 @@ package com.application.gameshelf;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("StartingPage.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        // Load the FXML file
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("hello-view.fxml"));
+        Parent root = fxmlLoader.load();
+
+        // Set the FXML content as the root of the scene
+        Scene scene = new Scene(root, 1440, 1024);
+
+        // Set the scene to the stage
         stage.setScene(scene);
+
+        // Set other stage properties if needed
+        stage.setTitle("Your Title");
+
+        // Show the stage
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
